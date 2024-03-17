2a8612626b2cae6fa35ad784bd5e2b8ec5c83a3e
vagrant <vagrant@debian12.localdomain>
2024-03-17 18:26:20 +0100

    exercice 3 bis

--- todolist-debut-ex2/src/test/java/com/acme/todolist/domain/TodoItemTest.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/domain/TodoItemTest.java
@@ -1,5 +1,44 @@
 package com.acme.todolist.domain;
 
+import org.testng.annotations.Test;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.testng.AssertJUnit.*;
+
 public class TodoItemTest {
-    
+
+    @Test
+    public void doitContenirLate() {
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime dueDate = now.minusHours(64);
+        String contenu = "Tester TodoItem";
+        Instant instantDueDate = dueDate.toInstant(ZoneOffset.UTC);
+        TodoItem todoItem = new TodoItem("111111111",instantDueDate, contenu);
+
+
+        String expected = "[LATE!] " + contenu;
+
+        assertTrue(todoItem.isLate());
+        assertEquals(expected, todoItem.finalContent());
+    }
+
+    @Test
+    public void neDoitPasContenirLate() {
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime dueDate = now.plusHours(64);
+        String contenu = "Tester TodoItem";
+        Instant instantDueDate = dueDate.toInstant(ZoneOffset.UTC);
+        TodoItem todoItem = new TodoItem("111111111",instantDueDate, contenu);
+
+        assertFalse(todoItem.isLate());
+        assertEquals(contenu, todoItem.finalContent());
+    }
+
 }
